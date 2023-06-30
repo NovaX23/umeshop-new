16bbd38547c5e37fb927e3fdcc11c285a09626d1
Mohammad Nouri <m.nouri.work@gmail.com>
2023-07-01 00:44:42 +0330

    Initial Commit

---
+++ .history/frontend/src/App_20230701001759.js
@@ -0,0 +1,16 @@
+
+
+function App () {
+  return (
+    <div >
+      <header>
+        <a href="/">Umeshop</a>
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App () {
+  return (
+    <div >
+      <header>
+        <a href="/">Umeshop</a>
+      </header>
+      <main>
+        List of Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
